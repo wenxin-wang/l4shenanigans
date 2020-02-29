b1a592de7e2d672c2eb77b695613592f2c20de58
Wenxin Wang <i@wenxinwang.me>
2020-02-29 23:45:44 +0800

    udp proxy protocol no seperate

--- build.sh
+++ build.sh
@@ -22,6 +22,7 @@ patch -d $builddir/$nginx_dir -Np1 -i $__DIR__/nginx-tproxy-patches/nginx-1.17.4
 patch -d $builddir/$nginx_dir -Np1 -i $__DIR__/nginx-tproxy-patches/nginx-1.17.4-netns-for-linux.patch
 patch -d $builddir/$nginx_dir -Np1 -i $__DIR__/nginx-tproxy-patches/nginx-1.17.4-udp-proxy-protocol.patch
 patch -d $builddir/$nginx_dir -Np1 -i $__DIR__/nginx-tproxy-patches/nginx-1.17.4-l4shenanigan.patch
+patch -d $builddir/$nginx_dir -Np1 -i $__DIR__/nginx-tproxy-patches/nginx-1.17.4-udp-proxy-protocol-no-seperate.patch
 
 cd $builddir/$nginx_dir
 CFLAGS="-g -O0" ./configure --with-compat --with-file-aio --with-http_gunzip_module --with-http_gzip_static_module --with-http_realip_module --with-http_secure_link_module --with-http_slice_module --with-http_ssl_module --with-http_stub_status_module --with-http_sub_module --with-http_v2_module --with-pcre-jit --with-stream --with-stream_realip_module --with-stream_ssl_module --with-stream_ssl_preread_module --with-threads --with-debug
